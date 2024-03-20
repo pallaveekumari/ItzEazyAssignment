582f5d5474aa4e28e6dfff6270cea23ea2c86527
pallaveekumari <pallaveeratnakar1714@gmail.com>
2024-03-20 11:02:40 +0100

    implemnted-the-options

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var totalInputs = $('.text-input, .select-box, .radio-input').length;
+    var filledInputs = 0;
+    var currentScreen = 1;
+
+    $('.text-input, .select-box, .radio-input').on('input change', function() {
+        filledInputs = 0;
+        $('.questions:visible').find('.text-input').each(function() {
+            if ($(this).val().length >= 3) {
+                filledInputs++;
+            }
+        });
+        $('.questions:visible').find('.select-box, .radio-input:checked').each(function() {
+            filledInputs++;
+        });
+        updateProgressBar();
+        // Check if all questions on the first screen are filled
+        if (filledInputs === totalInputs && currentScreen === 1) {
+            // Transition to the second screen
+            $('#screen1').hide();
+            $('#screen2').show();
+            currentScreen = 2;
+        }
+    });
+
+    function updateProgressBar() {
+        var percentage = (filledInputs / totalInputs) * 100;
+        $('.progress-bar').css('width', percentage + '%');
+    }
+});
